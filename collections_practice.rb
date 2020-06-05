f6f9b3681ee3eb9d381cc2164e94aa06c56fe416
Tanya Hodges <tanyac52003@gmail.com>
2020-06-05 17:43:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a<=>b 
+  end 
+end 
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b<=>a
+  end 
+end 
+
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length 
+  end 
+end 
+
+def swap_elements(array)
+  array.insert(2,array.delete_at(1))
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+end 
+
+def find_a(array)
+  new_array = []
+  new_array << array.find {|element| element[0] == "a"}
+end 
+
+def sum_array
+end
+
+def add_s
+end 
+  
+  
+  
+  
+  
+  
